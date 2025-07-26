a43dc1b823ff2eb06eb647bb642de1bc5b7626bb
M TEJ <94366226+tejgaming6@users.noreply.github.com>
2025-07-27 04:53:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
+resource "null_resource" "create_dirs_and_files" {
+  provisioner "local-exec" {
+    command = <<EOT
+      mkdir -p ./output/dir1 ./output/dir2
+      touch ./output/dir1/file1.txt
+      touch ./output/dir2/file2.txt
+    EOT
+  }
+}
 
