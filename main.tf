fd1678ea298dc6512f9d47af042a40224155b5a3
M TEJ <94366226+tejgaming6@users.noreply.github.com>
2025-07-27 19:41:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
-terraform {
-  required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.0"
-    }
-  }
 
-  required_version = ">= 0.13"
-}
 
 provider "null" {}
 
