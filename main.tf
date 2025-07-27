24c41dcb7a9f0d03e6a0f3a3939c40ef2853d596
M TEJ <94366226+tejgaming6@users.noreply.github.com>
2025-07-27 20:16:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
-
-
 provider "null" {}
 
 resource "null_resource" "create_dirs_and_files" {
   provisioner "local-exec" {
     command = <<TEJ
-      mkdir -p ./abc/dir1 ./abc/dir2
-      touch ./abc/dir1/file1.txt
-      touch ./abc/dir2/file2.txt
+      mkdir -p "${var.dir[0]}" "${var.dir[1]}"
+      touch "${var.file[0]}"
+      touch "${var.file[1]}"
     TEJ
   }
 }
